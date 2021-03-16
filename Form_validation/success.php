b2a1eeca7b4287e7d825aeda75bc9ced1c4ba5f0
shubhammindfire <shubhamk@mindfiresolutions.com>
2021-03-17 00:22:52 +0530

    Bug Fix: Added jQuery import for bootstrap

--- Form_validation/success.php
+++ Form_validation/success.php
@@ -8,6 +8,7 @@
     <title>Success page</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <link rel="stylesheet" type="text/css" href="./../assets/css/index.css" />
